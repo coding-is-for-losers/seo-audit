5be5c0ed65080daf4ea62892768acdc61700532b
The Commissioner <commissioner@codingisforlosers.com>
2019-10-01 14:49:16 -0500

    Updating actions based on Cesar feedback

--- models/metrics/actions/actions_proc.sql
+++ models/metrics/actions/actions_proc.sql
@@ -23,7 +23,7 @@ case
 case 
 	when indexable = false and found_at_sitemap is not null then concat('remove from sitemap: ', coalesce(found_at_sitemap) ) 
 	when indexable = true and found_at_sitemap is null then 'add to sitemap'
-	when robots_noindex is null and found_at_sitemap is null and url not like '%/page/%' and sessions_30d > 0 and http_status_code = 200 then 'add to sitemap'
+	when robots_noindex is null and found_at_sitemap is null and url not like '%/page/%' and sessions_30d > 0 and http_status_code = 200 then 'likely add to sitemap'
 	else '' end as sitemap_action,
 
 case 
