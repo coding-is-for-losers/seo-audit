fbd3a4ed19e198433743ec5024c08d2d9a254391
The Commissioner <commissioner@codingisforlosers.com>
2019-10-15 14:29:01 -0600

    Fan out crawls to prior / later months

--- models/admin/crawl_dates.sql
+++ models/admin/crawl_dates.sql
@@ -1,53 +1,59 @@
 with deepcrawl as (
 
-  SELECT
-  site,
-  crawl_date,
-  crawl_report_month,
-  max(previous_crawl_report_month) previous_crawl_report_month,
-  max(next_crawl_report_month) next_crawl_report_month
-  FROM (
-
 		SELECT
 		site,
 		crawl_date,
-		crawl_report_month,
-		lag(crawl_report_month, 1) over w1 as previous_crawl_report_month,
-		lead(crawl_report_month, 1) over w1 as next_crawl_report_month
+		crawl_report_month
 		FROM {{ ref('deepcrawl_proc') }}
-		WINDOW w1 as (partition by domain, site order by crawl_report_month asc)
-   )
-   GROUP BY site, crawl_date, crawl_report_month
+    GROUP BY site, crawl_date, crawl_report_month
 )
 
 SELECT
 site,
 run_date,
-max(crawl_date) crawl_date
+max(crawl_date) crawl_date,
+max(crawl_report_month) crawl_report_month
 FROM (
 
-	SELECT
-	a.site,
-	a.run_date,
-	CASE WHEN b.crawl_date is not null THEN b.crawl_date
-		WHEN c.crawl_date is not null THEN c.crawl_date
-		WHEN d.crawl_date is not null THEN d.crawl_date
-		ELSE null END as crawl_date
-	FROM {{ ref('dates')}} a
-	LEFT JOIN deepcrawl b
-	ON (
-		a.site = b.site AND
-		a.run_date = b.crawl_report_month
-	)
-	LEFT JOIN deepcrawl c
-	ON (
-		a.site = b.site AND
-		a.run_date = c.previous_crawl_report_month
-	)
-	LEFT JOIN deepcrawl d
-	ON (
-		a.site = b.site AND
-		a.run_date = d.next_crawl_report_month
-	)
+  SELECT
+  site,
+  run_date,
+  CASE WHEN crawl_date is not null THEN crawl_date
+    WHEN crawl_date_before_run_date is not null THEN crawl_date_before_run_date
+    WHEN crawl_date_after_run_date is not null THEN crawl_date_after_run_date  
+    ELSE null END as crawl_date,
+  CASE WHEN crawl_report_month is not null THEN crawl_report_month
+    WHEN crawl_report_month_before_run_date is not null THEN crawl_report_month_before_run_date
+    WHEN crawl_report_month_after_run_date is not null THEN crawl_report_month_after_run_date  
+    ELSE null END as crawl_report_month
+  FROM (
+      SELECT
+      a.site,
+      a.run_date,
+      b.crawl_date,
+      b.crawl_report_month,
+      c.crawl_date crawl_date_before_run_date,
+      c.crawl_report_month crawl_report_month_before_run_date,
+      d.crawl_date crawl_date_after_run_date,
+      d.crawl_report_month crawl_report_month_after_run_date
+      FROM {{ ref('dates') }} a
+      LEFT JOIN deepcrawl b
+      ON (
+        a.site = b.site and
+        a.run_date = b.crawl_report_month
+      )
+      LEFT JOIN deepcrawl c
+      ON (
+        a.site = c.site and
+        a.run_date > c.crawl_report_month
+      )
+      LEFT JOIN deepcrawl d
+      ON (
+        a.site = d.site and
+        a.run_date < d.crawl_report_month
+      )
+    )
 )
 GROUP BY site, run_date
+ORDER BY run_date desc
+  
